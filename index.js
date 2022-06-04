9c7b0c4746e8fcfa47c0a05b7d9171b055f2f0f1
wachira <wachiraa954@gmail.com>
2022-06-04 21:58:09 +0300

    submitting function parameters lab

---
+++ index.js
@@ -0,0 +1,8 @@
+function introduction (name){
+    return `Hi, my name is ${name}.`
+
+}
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
